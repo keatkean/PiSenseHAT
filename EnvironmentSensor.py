d9b305e30941d7d842ec5cb8e4fcd24088294197
Keat Kean <keatkean@gmail.com>
2025-01-10 10:31:02 +0800

    round the temp

--- EnvironmentSensor.py
+++ EnvironmentSensor.py
@@ -1,19 +1,22 @@
 from sense_hat import SenseHat
 
 sense = SenseHat()
+try:
+    while True:
+        t = sense.get_temperature()
+        t2 = sense.get_temperature_from_pressure()
+        p = sense.get_pressure()
+        h = sense.get_humidity()
 
-while True:
-    t = sense.get_temperature()
-    t2 = sense.get_temperature_from_pressure()
-    p = sense.get_pressure()
-    h = sense.get_humidity()
+        t = round(t,1)
+        t2 = round(t2,1)
+        p = round(p,1)
+        h = round(h,1)
 
-    t = round(t,1)
-    t2 = round(t2,1)
-    p = round(p,1)
-    h = round(h,1)
+        final_t = round((t+t2)/2,1)
 
-    final_t = (t+t2)/2
-
-    msg = "Temp: " + str(final_t) + " Humid: " + str(h)
-    sense.show_message(msg, scroll_speed=0.05)
+        msg = "Temp: " + str(final_t) + " Humid: " + str(h)
+        sense.show_message(msg, scroll_speed=0.05)
+except KeyboardInterrupt:
+    print('Application Terminated.')
+    sense.clear()
